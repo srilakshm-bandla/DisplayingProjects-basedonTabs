de36b4d47fb2d93e77aa96fca8df6f55dec64c8d
srilakshmi <srilakshmi.korlakunta@gmail.com>
2023-02-06 10:48:07 +0000

    displyaproject

---
+++ src/components/TabItem/index.js
@@ -0,0 +1,25 @@
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, updateActiveTabId, isActive} = props
+  const {displayText, tabId} = tabDetails
+
+  const onClickTabId = () => {
+    updateActiveTabId(tabId)
+  }
+  const activeTabClassName = isActive ? 'active-tab-btn' : ''
+
+  return (
+    <li className="tab-item-container ">
+      <button
+        type="button"
+        className={`tab-btn ${activeTabClassName}`}
+        onClick={onClickTabId}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
